c66a5250cd65cebca57245987ccfee0f92ced0ef
Leandro Santinho <leandro.santinho@outlook.com>
2021-03-03 19:59:09 -0300

    Projeto Quarkus

---
+++ src/main/java/com/usiminas/quarkus/GreetingResource.java
@@ -0,0 +1,24 @@
+package com.usiminas.quarkus;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello-resteasy")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+
+    @GET
+    @Path("Santinho")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String santinho() {
+        return "Santinho";
+    }
+
+}
